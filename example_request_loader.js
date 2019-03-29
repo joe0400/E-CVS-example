4ec3368acada382f882b8a8ddebf68ebe78c416b
joe0400 <joseph_scannell@student.uml.edu>
2019-03-28 20:56:59 -0400

    update

--- example_request_loader.js
+++ example_request_loader.js
@@ -13,8 +13,14 @@
 //	Written by Joseph Scannell
 //
 
+function parse(str) {
+    var args = [].slice.call(arguments, 1),
+        i = 0;
 
-const attributors = 100;
+    return str.replace(/%s/g, () => args[i++]);
+}
+
+const attributors = 312;
 var number = 0;
 
 var info = new Array();
@@ -25,44 +31,46 @@ var loremIpsum = "lorem ipsum dolor itum";
 var tvotes = [99,20,13,40,80];
 var dates = [1,8,2,13,12];
 
-for(var i = 0;i<5;i++){
-	info.push(new information(names[i],loremIpsum,tVotes[i],loremIpsum,usernames[i],dates[i]);
-}
-
 class information{
 	constructor(name,information,total_votes,description_text,username,date){
 		this.name = name;
 		this.information = information;
 		this.total_votes = total_votes;
-		this.description_text = descritpion_text;
+		this.description_text = description_text;
 		this.username = username;
 		this.toggle = false;
 		this.date = date;
 		this.number = number;
-		info.push(this);
-		this.generate();
-	}
-	
-	function viewName(){	return this.name;}
-	function viewInfo(){	return this.information;}
-	function viewVotes(){	return this.total_votes;}
-	function viewDescr(){	return this.description_text;}
-	function viewUser(){	return this.username;}
-	function viewDate(){	return this.date;)
+		this.viewName = function viewName(){	return this.name;};
+	this.viewInfo = function viewInfo(){	return this.information;};
+	this.viewVotes = function viewVotes(){	return this.total_votes;};
+	this.viewDescr = function viewDescr(){	return this.description_text;};
+	this.viewUser = function viewUser(){	return this.username;};
+	this.viewDate = function viewDate(){	return this.date;};
 
-	function increment(){
+	this.increment = function increment(){
 		if(this.toggle)
 			this.total_votes--;
 		else
 			this.total_votes++;
 		this.toggle = !this.toggle;
-		document.getElementByID(this.number.toString()).innerHTML = Number(this.viewVotes()/attributors).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
+		document.getElementById(this.number.toString()).innerHTML = Number(this.viewVotes()/attributors).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
+	};
+	
+	this.generate = function generate(){
+		var vr = Number(this.viewVotes()/attributors).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
+		var dat = "<li class=\"list-group-item\"><div class=\"d-flex w-100 justify-content-between\"><h5 class=\"mb-1\">%s</h5><small class=\"text-right text-nowrap\">%s days ago <u class=\"text-warning\" id=\"%s\" style=\"text-decoration: none !important;\">%s</u></small></div><small> @%s </small><p class=\"mb-1\">%s</p><div class=\"btn-group float-right\" role=\"group\" aria-label=\"Support\"><button type=\"button\" class=\"btn btn-primary\" data-toggle=\"button\" onclick=\"info[%s].increment()\" aria-pressed=\"false\" autocomplete=\"off\">Support</button></div></li>";
+	dat = parse(dat,this.name,this.date.toString(),this.number.toString(),vr.toString(),this.username,this.descrition_text,number.toString());
+		document.getElementById("inner lists").innerHTML += dat;
+
+	};
+		info.push(this);
+		this.generate();
+		number++;
 	}
 	
-	function generate(){
-		info = "<li class=\"list-group-item\"><div class=\"d-flex w-100 justify-content-between\"><h5 class=\"mb-1\">${this.name}</h5><small class=\"text-right text-nowrap\">${this.date} days ago <u class=\"text-warning\" id=\"${this.number}\" style=\"text-decoration: none !important;\"></u></small></div><small> @${this.username} </small><p class=\"mb-1\">${this.description_text}</p><div class=\"btn-group float-right\" role=\"group\" aria-label=\"Support\"><button type=\"button\" class=\"btn btn-primary\" data-toggle=\"button\" onclick=\"info[${number}].increment()\" aria-pressed=\"false\" autocomplete=\"off\">Support</button></div></li>";
-		document.getElementByID("inner lists").innerHTML += info;
-		document.getElementByID(this.number.toString()).innerHTML = Number(this.viewVotes()/attributors).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
-
-	}
 }
+for(var i = 0;i<5;i++){
+	new information(names[i],loremIpsum,tvotes[i],loremIpsum,usernames[i],dates[i]);
+}
+
